98c62446c0758329eb0e6e53b36882977bc3dcc6
Grazyna H <hudygrazyna@gmail.com>
2024-02-06 11:11:37 +0100

    update

--- main.py
+++ main.py
@@ -1,37 +1,22 @@
-import cv2
-from flask import Flask, request
-from flask_restful import Resource, Api
-from urllib.request import urlopen
-import numpy as np
+import classes as cl
+from flask import Flask, render_template
+from flask_restful import Api
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 api = Api(app)
 
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+@app.route('/')
+def index():
+    # html start template
+    return render_template('index.html')
 
 
-class PeopleCounter(Resource):
-    def get(self):
-        img = cv2.imread('images/droga.jpg')
-        boxes, weights = hog.detectMultiScale(img, winStride=(4, 4))
-        return {'count': len(boxes)}
+api.add_resource(cl.PeopleCounter1, '/1')
+api.add_resource(cl.PeopleCounter2, '/2/')
+api.add_resource(cl.PeopleCounter3, '/upload')
 
-
-class PeopleCounter2(Resource):
-    def get(self):
-        img_url = request.args.get('img_url')
-        # http://127.0.0.1:5000/?img_url=https://adres_zdjecia
-        resp = urlopen(img_url)
-        img2 = np.asarray(bytearray(resp.read()), dtype="uint8")
-        img2 = cv2.imdecode(img2, cv2.IMREAD_COLOR)
-        boxes, weights = hog.detectMultiScale(img2, winStride=(4, 4))
-        return {'count': len(boxes)}
-
-
-api.add_resource(PeopleCounter, '/people')
-api.add_resource(PeopleCounter2, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
